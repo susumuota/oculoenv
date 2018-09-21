e66fef5799b4c61cbab29eb1b8ac1bd19f4d4214
Susumu OTA <>
2018-09-21 12:26:21 +0900

    added retina image options.

--- oculoenv/environment.py
+++ oculoenv/environment.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 
 import sys
 import numpy as np
+import cv2
 
 import pyglet
 from pyglet.gl import *
@@ -140,7 +141,7 @@ class Camera(object):
 class Environment(object):
     """ Task Environmenet class. """
 
-    def __init__(self, content, off_buffer_width=128, on_buffer_width=640, skip_red_cursor=False):
+    def __init__(self, content, off_buffer_width=128, on_buffer_width=640, skip_red_cursor=False, retina=False):
         """ Oculomotor task environment class.
 
         Arguments:
@@ -149,7 +150,16 @@ class Environment(object):
           on_buffer_width: (int) pixel width and height size of display window.
         """
 
+        # skip
         self.skip_red_cursor = skip_red_cursor
+
+        # retina images
+        self.retina = retina
+        if self.retina:
+            self.on_blur_rates, self.on_inv_blur_rates = self._create_rate_datas(on_buffer_width) # 640
+            self.on_gray_rates, self.on_inv_gray_rates = self._create_rate_datas(on_buffer_width, gain=0.5) # 640
+            self.off_blur_rates, self.off_inv_blur_rates = self._create_rate_datas(off_buffer_width) # 128
+            self.off_gray_rates, self.off_inv_gray_rates = self._create_rate_datas(off_buffer_width, gain=0.5) # 128
 
         # Invisible window to render into (shadow OpenGL context)
         self.shadow_window = pyglet.window.Window(
@@ -270,7 +280,10 @@ class Environment(object):
         pass
 
     def _render_offscreen(self):
-        return self._render_sub(self.frame_buffer_off)
+        img = self._render_sub(self.frame_buffer_off)
+        if self.retina:
+            img = self._create_retina_image(img, self.off_blur_rates, self.off_inv_blur_rates, self.off_gray_rates, self.off_inv_gray_rates)
+        return img
 
     def render(self, mode='human', close=False):
         if close:
@@ -279,6 +292,10 @@ class Environment(object):
             return
 
         img = self._render_sub(self.frame_buffer_on)
+
+        if self.retina:
+            img = self._create_retina_image(img, self.on_blur_rates, self.on_inv_blur_rates, self.on_gray_rates, self.on_inv_gray_rates)
+
         if mode == 'rgb_array':
             return img
 
@@ -371,16 +388,93 @@ class Environment(object):
 
         return frame_buffer.read()
 
+    # copied from https://github.com/wbap/oculomotor/blob/master/application/functions/retina.py
+    def _gauss(self, x, sigma):
+        sigma_sq = sigma * sigma
+        return 1.0 / np.sqrt(2.0 * np.pi * sigma_sq) * np.exp(-x*x/(2 * sigma_sq))
+
+    def _create_rate_datas(self, width, sigma=0.32, clipping_gain=1.2, gain=1.0):
+        """ Create mixing rate.
+        Arguments:
+            width: (int) width of the target image.
+            sigma: (float) standard deviation of the gaussian.
+            clipping_gain: (float) To make the top of the curve flat, apply gain > 1.0
+            gain: (float) Final gain for the mixing rate.
+                          e.g.) if gain=0.8, mixing rates => 0.2~1.0
+        Returns:
+            Float ndarray (128, 128, 1): Mixing rates and inverted mixing rates.
+        """
+        rates = [0.0] * (width * width)
+        hw = width // 2
+        for i in range(width):
+            x = (i - hw) / float(hw)
+            for j in range(width):
+                y = (j - hw) / float(hw)
+                r = np.sqrt(x*x + y*y)
+                rates[j*width + i] = self._gauss(r, sigma=sigma)
+        rates = np.array(rates)
+        # Normalize
+        rates = rates / np.max(rates)
+
+        # Make top flat by multipying and clipping
+        rates = rates * clipping_gain
+        rates = np.clip(rates, 0.0, 1.0)
+
+        # Apply final gain
+        if gain != 1.0:
+            rates = rates * gain + (1-gain)
+        rates = rates.reshape([width, width, 1])
+        inv_rates = 1.0 - rates
+        return rates, inv_rates
+
+    def _create_blur_image(self, image):
+        h = image.shape[0]
+        w = image.shape[1]
+
+        # Resizeing to 1/2 size
+        resized_image0 = cv2.resize(image,
+                                  dsize=(h//2, w//2),
+                                  interpolation=cv2.INTER_LINEAR)
+        # Resizeing to 1/4 size
+        resized_image1 = cv2.resize(resized_image0,
+                                  dsize=(h//4, w//4),
+                                  interpolation=cv2.INTER_LINEAR)
+        # Resizeing to 1/8 size
+        resized_image2 = cv2.resize(resized_image1,
+                                  dsize=(h//8, w//8),
+                                  interpolation=cv2.INTER_LINEAR)
+
+        # Resizing to original size
+        blur_image = cv2.resize(resized_image2,
+                                dsize=(h, w),
+                                interpolation=cv2.INTER_LINEAR)
+
+        # Conver to Grayscale
+        gray_blur_image = cv2.cvtColor(blur_image, cv2.COLOR_BGR2GRAY)
+        gray_blur_image = np.reshape(gray_blur_image,
+                                     [gray_blur_image.shape[0],
+                                      gray_blur_image.shape[0], 1])
+        gray_blur_image = np.tile(gray_blur_image, 3)
+        return blur_image, gray_blur_image
+
+    def _create_retina_image(self, image, blur_rates, inv_blur_rates, gray_rates, inv_gray_rates):
+        blur_image, gray_blur_image = self._create_blur_image(image)
+        # Mix original and blur image
+        blur_mix_image = image * blur_rates + blur_image * inv_blur_rates
+        # Mix blur mixed image and gray blur image.
+        gray_mix_image = blur_mix_image * gray_rates + gray_blur_image * inv_gray_rates
+        return gray_mix_image.astype(np.uint8)
+
 
 class RedCursorEnvironment(Environment):
     # TODO: 0.5 is enough?
     CAMERA_HORIZONTAL_ANGLE_RAND_MAX = 1.0 * CAMERA_HORIZONTAL_ANGLE_MAX
     CAMERA_VERTICAL_ANGLE_RAND_MAX = 1.0 * CAMERA_VERTICAL_ANGLE_MAX
 
-    def __init__(self, content, off_buffer_width=128, on_buffer_width=640, skip_red_cursor=False):
+    def __init__(self, content, off_buffer_width=128, on_buffer_width=640, skip_red_cursor=False, retina=False):
         assert content == None # ignore content!!!
         assert skip_red_cursor == False
-        super().__init__(PointToTargetContent(), off_buffer_width, on_buffer_width, False)
+        super().__init__(PointToTargetContent(), off_buffer_width, on_buffer_width, False, retina)
         self.reaction_step = 0
 
     def step(self, action):
